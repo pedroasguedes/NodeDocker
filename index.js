9a840b00e4d9edd680f6a8c82bd3e841f39f4320
Pedro Guedes <pedroguedes583@gmail.com>
2022-08-16 16:41:37 -0300

    1608

--- index.js
+++ index.js
@@ -22,6 +22,47 @@ conexao.connect((erro)=>{
 
 app.get("/usuarios/listar",(req, res) => {
     conexao.query("select * from usuarios",(erro,dados) => {
-        
-    })
+      if (erro)return res.status(500).send({output:`Erro -> ${erro}`});
+      res.status(200).send({output:dados});  
+    });
+});
+
+app.post("/usuarios/cadastrar",(req, res) => {
+    if(req.body.nomeusuarios=="" || req.body.senha == "" 
+    || req.body.email == "" 
+    || req.body.nomecompleto == "" 
+    || req.body.cpf == "" 
+    ||req.body.foto == ""){
+        return res.status(400).send({output:`você deve digitar todos os dados`});
+    }
+    conexao.query("insert into usuarios set ?",req.body,(error, data)=>{
+        if(erro)return res.status(500).send({output: `Erro ao tentar cadastrar ->  ${erro}`})
+        res.status(201).send({output:`Usuario cadastrado`,dados:data});
+    });
+app.post("/usuarios/login",(req,res)=>{
+    if(req.body.nomeusuarios=="" 
+    || req.body.senha == "" 
+    
+    ){
+        return res.status(400).send({output:`Você deve passar todos os dados`});
+    }
+    conexao.query(`select * from usuarios where nomeusuarios=? and senha=?`,[req.body.nomeusuarios,req.body.senha],
+        (error, data)=>{
+            if(erro)return res.status(500).send({output:`Erro ao tentar logar -> ${erro}`})
+            res.status(200).send({output:`Logado`,dados:data})
+        }
+    )
+});
+});
+
+
+app.put("/usuarios/atualizar/:id",(req, res)=>{
+    conexao.query("update usuario set ? where id=?",[req.body.req.params.id], (error,data)=>{
+        if(error)
+        return res.status(500).send({output:`Erro ao tentar atualizar -> ${erro}`,
+    });
+    res.status(200).send({output:`Atualizar`,dados:data});
+    });
 })
+
+app.listen("3000",() => console.log("Servidor online"));
